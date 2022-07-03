6d8be68d520b33c57141b3030ccc44420af510c7
AtanasStoyanov99 <atanas.stoyanov1199@gmail.com>
2022-07-04 01:23:28 +0300

    Add persistence of redux state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistCombineReducers } from 'reduxjs-toolkit-persist';
+import storage from 'reduxjs-toolkit-persist/lib/storage';
 import authenticationReducer from "./authenticationSlice";
-import channelDataReducer from "./channelDataSlice"
+import channelDataReducer from "./channelDataSlice";
 
-export const store = configureStore({
-    reducer: {
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+};
+
+const persistedReducer = persistCombineReducers(
+    persistConfig, 
+    {
         authentication: authenticationReducer,
         channelData: channelDataReducer
     }
+); 
+
+export const store = configureStore({
+    reducer: persistedReducer
 });
